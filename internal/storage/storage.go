ba6f8be4172bbb5d0f2ebe5d4565a7478af23c61
unknown <amit.aditya.1807@gmail.com>
2025-03-12 17:20:14 +0530

    add get students list

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -5,4 +5,5 @@ import "github.com/amitaditya1807/students-api/internal/types"
 type Storage interface {
 	CreateStudent(name string, email string, age int) (int64, error)
 	GetStudentById(id int64) (types.Student, error)
+	GetStudents() ([]types.Student, error)
 }
